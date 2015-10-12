81987b44d8ff93664a69a18447008068726416a2
Prashant Ghimire <prashant.ghimire@selu.edu>
2015-10-12 14:52:40 -0500

    update

---
+++ application/views/home.php
@@ -0,0 +1,71 @@
+<div class="row">
+    <div class="col-md-6 img-pri">
+        <img src="http://thenextweb.com/wp-content/blogs.dir/1/files/2015/06/Apple-News-1200x600.jpg" alt="">
+    </div>
+    <div class="col-md-6 img-sec">
+        <img src="http://thenextweb.com/wp-content/blogs.dir/1/files/2015/10/waterbug-Facebook-Like.png" alt="">
+    </div>
+    <div class="col-md-3 img-ter">
+        <img src="http://thenextweb.com/wp-content/blogs.dir/1/files/2015/10/Halloween-Social.gif" alt="">
+    </div>
+    <div class="col-md-3 img-ter">
+        <img src="http://thenextweb.com/wp-content/blogs.dir/1/files/2015/06/shutterstock_200260748_Nintendo_Mario-1200x600.jpg" alt="">
+    </div>
+</div>
+<br><br>
+<div class="row">
+    <div class="alert alert-danger">
+        <p class="center">
+            <strong>ANNOUNCEMENT</strong> TNW Conference in New York: Our lineup is almost complete, check it out!
+        </p>
+    </div>
+</div>
+
+
+<div class="row thumb-posts">
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2015/10/Screen-Shot-2015-10-09-at-8.32.22-PM-520x296.png" alt="">
+            <div class="caption"><p>I love you baby, hehe!</p></div>
+        </div>
+    </div>
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2015/10/3d-printing-520x261.jpg" alt="">
+            <div class="caption"><p>I love you baby, hehe!</p></div>
+        </div>
+    </div>
+
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2015/10/Halloween-Social-520x286.gif" alt="">
+            <div class="caption"><p>I love you baby, hehe!</p></div>
+        </div>
+    </div>
+
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2015/06/Apple-News-520x260.jpg" alt="">
+            <div class="caption"><p>I love you baby, hehe!</p></div>
+        </div>
+    </div>
+
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/08/books.png" alt="">
+            <div class="caption"><p>I love you baby, hehe!</p></div>
+        </div>
+    </div>
+
+    <div class="col-md-2">
+        <div class="thumbnail">
+            <img src="http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2015/10/FoxType-520x260.jpg" alt="">
+            <div class="caption"><p>I love you baby, hehe!</p></div>
+        </div>
+    </div>
+
+
+
+
+
+</div>
---
+++ application/views/shared/header.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <title>Something to Write</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <?= link_tag('lib/css/style.css'); ?>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <?= link_tag('lib/js/scripts.js');?>
+    </head>
+    <body class="main-body">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <!-- We use the fluid option here to avoid overriding the fixed width of a normal container within the narrow content columns. -->
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-6" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand name" href="#"><?= img('lib/images/logo.png') ?> <span>STW</span> <i class="glyphicon glyphicon-chevron-down"></i></a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-6">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Apps</a></li>
+                    <li><a href="#">Gear</a></li>
+                    <li><a href="#">Tech</a></li>
+                    <li><a href="#">Creative</a></li>
+                    <li><a href="#">Money</a></li>
+                    <li><a href="#">Insights</a></li>
+                    <li><a href="#">Launch</a></li>
+                    <li><a href="#">World</a></li>
+                    <li><a href="#">Distract</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div>
+    </nav>
